d7146ac7e6c807c69c57a3c15b51b515a6a1e13c
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2025-05-09 15:26:25 +0200

    Added an example insertion

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import sqlite3
 import os
 
-
 if __name__ == "__main__":
     # Connect to the SQLite database
-    conn = sqlite3.connect('example.db')
+    conn = sqlite3.connect('warehouse.db')
 
     # Create a cursor object
     cursor = conn.cursor()
@@ -12,28 +11,27 @@ if __name__ == "__main__":
     # Create a table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS products (
-            sku INTEGER PRIMARY KEY,
-            batch_number INTEGER PRIMARY KEY,
+            sku INTEGER,
+            batch_number INTEGER,
             total_cost REAL,
-            measurement TEXT,
+            measurement CHAR(32),
 			nr_of INTEGER,
-            exp_date DATE
+            exp_date DATE,
+            PRIMARY KEY (sku, batch_number)
         )
     ''')
 
-
-    # Ex:
-    # Insert a row of data
+    # Ex: Insert a row of data
     cursor.execute('''
-        INSERT INTO users (name, age)
-        VALUES ('Alice', 30)
+        INSERT INTO products (sku, batch_number, total_cost, measurement, nr_of, exp_date)
+        VALUES (123456, 1, 19.99, 'kg', 10, '2024-12-31')
     ''')
 
     # Save (commit) the changes
     conn.commit()
 
     # Query the database
-    cursor.execute('SELECT * FROM users')
+    cursor.execute('SELECT * FROM products')
     rows = cursor.fetchall()
 
     for row in rows:
